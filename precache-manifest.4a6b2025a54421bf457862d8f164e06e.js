c5fdd291d5a35fe2710c50442fb90e457b32c05e
Isabella Chew <izzycsy@gmail.com>
2020-09-03 17:03:55 +0800

    Updates

---
+++ precache-manifest.4a6b2025a54421bf457862d8f164e06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01c3edb09ab3117121adb94c5b8afd7",
+    "url": "/covidashboard/index.html"
+  },
+  {
+    "revision": "cfbb3b132e43636467d1",
+    "url": "/covidashboard/static/css/main.e58bb2b8.chunk.css"
+  },
+  {
+    "revision": "f0031c3bf81c81ed4171",
+    "url": "/covidashboard/static/js/2.58cc850a.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covidashboard/static/js/2.58cc850a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfbb3b132e43636467d1",
+    "url": "/covidashboard/static/js/main.152d9f31.chunk.js"
+  },
+  {
+    "revision": "fb4ad5f86a12b9fee794",
+    "url": "/covidashboard/static/js/runtime-main.3878c2df.js"
+  }
+]);
